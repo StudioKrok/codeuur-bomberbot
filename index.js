bcf0f3e7abdeb28b1f69c198419902cf5f848ba8
agares <ekeisco@gmail.com>
2015-02-15 23:55:53 -0500

    with typos

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var express = require('express')
+var engines = require('consolidate');
 var app = express()
 
 app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
 
 app.get('/', function (req, res) {
-  res.render('index.html');
-})
+  console.log('no entra por aca');
+  res.render('index');
+});
+
+
+app.get('/temp', function (req, res) {
+  res.render('temp');
+});
+
 
 var server = app.listen(8083, function () {
 
