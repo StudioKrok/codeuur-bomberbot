b2c2a8f3dccdbd0a1ef1f183de95f3870c493654
agares <ekeisco@gmail.com>
2015-02-16 01:06:06 -0500

    ends configuration

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express')
 var engines = require('consolidate');
+var GoogleSpreadsheet = require("google-spreadsheet");
+var my_sheet = new GoogleSpreadsheet('<key>');
+var bodyParser = require('body-parser');
 var app = express()
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/public');
@@ -8,14 +14,35 @@ app.engine('html', engines.mustache);
 app.set('view engine', 'html');
 
 app.get('/', function (req, res) {
-  console.log('no entra por aca');
   res.render('index');
+});
+
+app.post('/', function (req, res) {
+  console.log(req.body);
+  my_sheet.setAuth('mail@gmail.com', 'password', function(err){
+    my_sheet.getRows(1, function(err, rows){
+      my_sheet.addRow('worksheet_id', {
+        date: new Date(),
+        name: req.body.name,
+        school: req.body.school,
+        email: req.body.email,
+        message: req.body.message
+      }, function(err){
+        if(err){
+          console.log('err', err);
+        }
+        res.redirect('/');
+      });
+    })
+
+  });
 });
 
 
 app.get('/temp', function (req, res) {
-  res.render('temp');
 });
+
+
 
 
 var server = app.listen(8083, function () {
